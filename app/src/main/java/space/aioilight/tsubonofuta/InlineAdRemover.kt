8df2e8fd56106b0aafbe9285e37de8d4fb5727f1
AioiLight <info@aioilight.space>
2022-03-09 21:47:16 +0900

    Initial commit

---
+++ app/src/main/java/space/aioilight/tsubonofuta/InlineAdRemover.kt
@@ -0,0 +1,30 @@
+package space.aioilight.tsubonofuta
+
+import de.robv.android.xposed.XC_MethodHook
+import de.robv.android.xposed.XposedBridge
+import de.robv.android.xposed.XposedHelpers
+import de.robv.android.xposed.callbacks.XC_LoadPackage
+import java.lang.Exception
+
+class InlineAdRemover {
+    fun RemoveInlineAd(lpparam: XC_LoadPackage.LoadPackageParam)
+    {
+        try {
+            XposedBridge.hookAllMethods(
+                XposedHelpers.findClass(
+                    "jp.syoboi.a2chMate.view.ad.InlineAdContainer", lpparam.classLoader),
+                "isAvailableInlineAd",
+                object : XC_MethodHook() {
+                    public override fun afterHookedMethod(param: MethodHookParam?) {
+                        super.afterHookedMethod(param)
+                        if (param != null) {
+                            param.result = false
+                        }
+                    }
+                })
+        }
+        catch (e: Exception) {
+            XposedBridge.log("Cannot hook method of inline ads.")
+        }
+    }
+}
---
+++ app/src/main/java/space/aioilight/tsubonofuta/Yabaiwayo.kt
@@ -0,0 +1,15 @@
+package space.aioilight.tsubonofuta
+
+import de.robv.android.xposed.IXposedHookLoadPackage
+import de.robv.android.xposed.callbacks.XC_LoadPackage
+
+class layP : IXposedHookLoadPackage {
+    override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam?) {
+        if (lpparam != null) {
+            if (lpparam.packageName == "jp.co.airfront.android.a2chMate")
+            {
+                InlineAdRemover().RemoveInlineAd(lpparam)
+            }
+        }
+    }
+}

4863496fcb45f19ce74c4b3996829e99f9b91d2a
AioiLight <info@aioilight.space>
2022-03-12 20:48:01 +0900

    Adapt ThreadAdRemover to Chmate 0.8.10.152

--- app/src/main/java/space/aioilight/tsubonofuta/ThreadAdRemover.kt
+++ app/src/main/java/space/aioilight/tsubonofuta/ThreadAdRemover.kt
@@ -1,6 +1,7 @@
 package space.aioilight.tsubonofuta
 
 import de.robv.android.xposed.XC_MethodHook
+import de.robv.android.xposed.XC_MethodReplacement
 import de.robv.android.xposed.XposedBridge
 import de.robv.android.xposed.XposedHelpers
 import de.robv.android.xposed.callbacks.XC_LoadPackage
@@ -23,53 +24,74 @@ class ThreadAdRemover {
                 XposedHelpers.findClass(
                     view,
                     lpparam.classLoader),
-                "reload",
+                "setNgWordExists",
                 object: XC_MethodHook () {
                     override fun beforeHookedMethod(param: MethodHookParam?) {
-                        if (!isHome(param)) {
-                            param?.result = null
-                        }
+                        param?.result = null
+//                        if (!isHome(param)) {
+//                            param?.result = null
+//                        }
                     }
                 })
+//            XposedBridge.hookAllConstructors(
+//                XposedHelpers.findClass(view, lpparam.classLoader),
+//                object: XC_MethodReplacement () {
+//                    override fun replaceHookedMethod(param: MethodHookParam?) {
+//                        // Do nothing
+//                    }
+//                }
+//            )
+//            XposedBridge.hookAllMethods(
+//                XposedHelpers.findClass(
+//                    view,
+//                    lpparam.classLoader),
+//                "reload",
+//                object: XC_MethodHook () {
+//                    override fun beforeHookedMethod(param: MethodHookParam?) {
+//                        if (!isHome(param)) {
+//                            param?.result = null
+//                        }
+//                    }
+//                })
 
-            XposedBridge.hookAllMethods(
-                XposedHelpers.findClass(
-                    view,
-                    lpparam.classLoader),
-                "onDestroy",
-                object: XC_MethodHook () {
-                    override fun beforeHookedMethod(param: MethodHookParam?) {
-                       if (!isHome(param)) {
-                            param?.result = null
-                        }
-                    }
-                })
-
-            XposedBridge.hookAllMethods(
-                XposedHelpers.findClass(
-                    view,
-                    lpparam.classLoader),
-                "onPause",
-                object: XC_MethodHook () {
-                    override fun beforeHookedMethod(param: MethodHookParam?) {
-                        if (!isHome(param)) {
-                            param?.result = null
-                        }
-                    }
-                })
-
-            XposedHelpers.findAndHookMethod(
-                XposedHelpers.findClass(
-                    view,
-                    lpparam.classLoader),
-                "onResume",
-                object: XC_MethodHook () {
-                    override fun beforeHookedMethod(param: MethodHookParam?) {
-                        if (!isHome(param)) {
-                            param?.result = null
-                        }
-                    }
-                })
+//            XposedBridge.hookAllMethods(
+//                XposedHelpers.findClass(
+//                    view,
+//                    lpparam.classLoader),
+//                "onDestroy",
+//                object: XC_MethodHook () {
+//                    override fun beforeHookedMethod(param: MethodHookParam?) {
+//                       if (!isHome(param)) {
+//                            param?.result = null
+//                        }
+//                    }
+//                })
+//
+//            XposedBridge.hookAllMethods(
+//                XposedHelpers.findClass(
+//                    view,
+//                    lpparam.classLoader),
+//                "onPause",
+//                object: XC_MethodHook () {
+//                    override fun beforeHookedMethod(param: MethodHookParam?) {
+//                        if (!isHome(param)) {
+//                            param?.result = null
+//                        }
+//                    }
+//                })
+//
+//            XposedHelpers.findAndHookMethod(
+//                XposedHelpers.findClass(
+//                    view,
+//                    lpparam.classLoader),
+//                "onResume",
+//                object: XC_MethodHook () {
+//                    override fun beforeHookedMethod(param: MethodHookParam?) {
+//                        if (!isHome(param)) {
+//                            param?.result = null
+//                        }
+//                    }
+//                })
         }
         catch (e: Exception) {
             XposedBridge.log("Cannot hook method of thread ad.")

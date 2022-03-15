db566fb8a053c9ef038928e18a58ae00e6069685
AioiLight <info@aioilight.space>
2022-03-15 22:06:13 +0900

    Fix unable to remove inline ads on 0.8.10.153

--- app/src/main/java/space/aioilight/tsubonofuta/InlineAdRemover.kt
+++ app/src/main/java/space/aioilight/tsubonofuta/InlineAdRemover.kt
@@ -1,5 +1,8 @@
 package space.aioilight.tsubonofuta
 
+import android.content.Context
+import android.view.View
+import android.view.ViewGroup
 import de.robv.android.xposed.XC_MethodHook
 import de.robv.android.xposed.XposedBridge
 import de.robv.android.xposed.XposedHelpers
@@ -10,17 +13,30 @@ class InlineAdRemover {
     fun removeInlineAd(lpparam: XC_LoadPackage.LoadPackageParam)
     {
         try {
-            XposedBridge.hookAllMethods(
-                XposedHelpers.findClass(
-                    "jp.syoboi.a2chMate.view.ad.InlineAdContainer", lpparam.classLoader),
-                "isAvailableInlineAd",
+            XposedHelpers.findAndHookMethod(
+                XposedHelpers.findClass("o.fireExposureChange", lpparam.classLoader),
+                "RemoteActionCompatParcelizer",
+                "android.view.View",
+                "android.view.ViewGroup",
                 object : XC_MethodHook() {
-                    public override fun afterHookedMethod(param: MethodHookParam?) {
+                    override fun beforeHookedMethod(param: MethodHookParam?) {
                         if (param != null) {
-                            param.result = false
+                            param.result = View((param.args[1] as ViewGroup).context)
                         }
                     }
-                })
+                }
+            )
+//            XposedBridge.hookAllMethods(
+//                XposedHelpers.findClass(
+//                    "jp.syoboi.a2chMate.view.ad.InlineAdContainer", lpparam.classLoader),
+//                "c",
+//                object : XC_MethodHook() {
+//                    public override fun beforeHookedMethod(param: MethodHookParam?) {
+//                        if (param != null) {
+//                            param.result = null
+//                        }
+//                    }
+//                })
         }
         catch (e: Exception) {
             XposedBridge.log("Cannot hook method of inline ads.")
